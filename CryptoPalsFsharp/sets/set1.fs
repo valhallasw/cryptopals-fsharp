a7a2b8f1213b21a1770b075bd45c802fb98479cb
Merlijn van Deen <valhallasw@arctus.nl>
2023-07-01 23:19:58 +0200

    Challenge 6

--- CryptoPalsFsharp/sets/set1.fs
+++ CryptoPalsFsharp/sets/set1.fs
@@ -71,6 +71,11 @@ let challenge4 () = // Detect single-character XOR
     lines |> Seq.collect (crackXorOptions keys) |>
              Seq.maxBy (score) |>
              should equal "Now that the party is jumping\n"
+       
+    let (s, k, r) = lines |> List.map crackXor |> List.maxBy (fun (score, _, _) -> score)
+    s |> should equal (String.length "Now that the party is jumping")  // newline is considered non-printable
+    k |> should equal 0x35
+    r |> should equal "Now that the party is jumping\n"
 
 let repeatkey_list (key: list<'T>) = Seq.initInfinite (fun index -> key[index % key.Length])
 let repeatkey (key: seq<'T>) = key |> List.ofSeq |> repeatkey_list
@@ -81,13 +86,86 @@ let testRepeat () =
     "ABC" |> Seq.map Ascii.charToVal |> repeatkey |> Seq.take 4 |> Seq.map Ascii.valToChar |> should equal "ABCA"
     "ABC" |> Seq.map Ascii.charToVal |> repeatkey |> Seq.take 6 |> Seq.map Ascii.valToChar |> should equal "ABCABC"
 
+let xorRepeating key content =
+    Seq.zip content (key |> repeatkey) |> Seq.map((<||) xor)
+
 [<Test>]
 let challenge5 () = // Implement repeating-key XOR
     let content = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal" |> Seq.map Ascii.charToVal
     let key = "ICE" |> Seq.map Ascii.charToVal
     
-    let encryptedhex = Seq.zip content (key |> repeatkey) |>
-        Seq.map((<||) xor) |>
-        Hex.byteToHex
+    let encrypted = content |> xorRepeating key
     
-    encryptedhex |> should equal "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+    encrypted |> Hex.byteToHex |> should equal "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+let hammingdistance (a: seq<int>) (b: seq<int>) =
+    Seq.zip a b |>
+    Seq.map((<||) xor) |>
+    Bits.bytesToBits |>
+    Seq.sum
+    
+[<Test>]
+let test_hammingdistance () =
+    hammingdistance ("this is a test" |> Seq.map Ascii.charToVal) ("wokka wokka!!!" |> Seq.map Ascii.charToVal) |>
+    should equal 37
+
+[<Test>]
+let testGroupBy () =
+    let input = [ (1, 0); (2, 1); (3, 0); (4, 1) ]
+    input |> Seq.groupBy snd |> should equal [ (0, [ (1,0); (3,0) ]); (1, [ (2,1); (4,1) ]) ]
+
+// let grouper size input =
+//     Seq.zip input (seq { 0 .. 1 .. (size - 1) } |> repeatkey) |>  // [ (input[i], i % size); ...]
+//     Seq.groupBy snd |>                                            // [ (0, [ (input[0], 0); (input[0 + size], 0); ... ]); ... ]
+//     Seq.map snd |>                                                // [ [ (input[0], 0); (input[0 + size], 0); ... ], ... ];
+//     Seq.map (Seq.map fst)                                         // [ [ input[0]; input[0 + size], ... ], ... ]
+    
+let grouper size input =
+    let modSize v = v % size
+    input |> Seq.indexed |> Seq.groupBy (fst >> modSize) |> Seq.map snd |> Seq.map (Seq.map snd)
+
+[<Test>]
+let testGrouper () =
+    seq { 10 .. 1 .. 15 } |> grouper 1 |> should equal [ [10; 11; 12; 13; 14; 15 ]; ]
+    seq { 10 .. 1 .. 15 } |> grouper 2 |> should equal [ [10; 12; 14]; [11; 13; 15] ]
+    seq { 10 .. 1 .. 15 } |> grouper 3 |> should equal [ [10; 13]; [11; 14]; [12; 15] ]
+    seq { 10 .. 1 .. 15 } |> grouper 4 |> should equal [ [10; 14]; [11; 15]; [12;]; [13;] ]
+    
+let crackXorWithSize keySize bytes =
+    let grouped = grouper keySize bytes
+    let cracked_key = grouped |> Seq.map (Seq.toList >> crackXor >> (fun (_, k, _) -> k)) |> Seq.toList
+    let decoded = xorRepeating cracked_key bytes |> Ascii.byteToChars
+    (Ascii.countprintable decoded, cracked_key, decoded)
+
+[<Test>]
+let challenge6 () = // Break repeating-key XOR
+    let bytes = File.readChallengeData "6.txt" |> String.concat "" |> Base64.base64ToByte
+    
+    // first determine the keysize
+    let keysizes = { 2 .. 1 .. 40 }
+    
+    let fldiv x y = (double x) / (double y)
+    let ks_hamming ks shift = fldiv (hammingdistance (bytes |> Seq.skip shift |> Seq.take ks) (bytes |> Seq.skip (ks + shift) |> Seq.take ks)) ks
+    let ks_hamming ks = (ks_hamming ks 0) + (ks_hamming ks ks) + (ks_hamming ks (2*ks)) + (ks_hamming ks (3*ks))
+    
+    let keysizes_likely = keysizes |>
+                          Seq.map ( fun (ks: int) -> (ks, ks_hamming ks)) |>
+                          Seq.sortBy snd |>
+                          Seq.map fst |>
+                          Seq.take 3
+    
+    keysizes_likely |> should equal [ 2; 5; 29 ]
+    
+    // for ks in keysizes_likely do
+    //     let grouped = grouper ks bytes
+    //     let cracked_key = grouped |> Seq.map (Seq.toList >> crackXor >> (fun (_, k, _) -> k)) |> Seq.toList
+    //     let decoded = xorRepeating cracked_key bytes |> Seq.take 32 |> Ascii.byteToChars
+    //     printfn $"{ks}, {cracked_key |> Hex.byteToHex}, {decoded}"
+    
+    let (score, cracked_key, decoded) = keysizes_likely |> Seq.map (fun ks -> crackXorWithSize ks bytes) |> Seq.maxBy (fun (s, _, _) -> s)
+    
+    printfn $"{decoded}"
+
+    cracked_key |> Hex.byteToHex |> should equal "5465726d696e61746f7220583a204272696e6720746865206e6f697365"
+    decoded[0..32] |> should equal "I'm back and I'm ringin' the bell"
+
